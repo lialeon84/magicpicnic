d5139e75a79dbc5778578d64b6c189ad2c4f66a8
Rose Vergel <50724307+lialeon84@users.noreply.github.com>
2021-03-14 20:57:56 -0500

    saving

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+fs.readFile("index.html", (err, html) => {
+    if (err) {
+        throw err;
+    }
+    const server = http.createServer(function(req, res) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(html);
+        res.end();
+    });
+
+    server.listen(port, hostname, function() {
+        console.log("Server running at http://" + hostname + ":" + port + "/");
+    });
+});
