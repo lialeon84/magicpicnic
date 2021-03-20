1ce29001d827c61659bb937cba6b8070ac4d2f23
Rose Vergel <50724307+lialeon84@users.noreply.github.com>
2021-03-20 17:16:58 -0500

    added some new features

---
+++ app.js
@@ -0,0 +1,9 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { createPopper } from "@popperjs/core";
+const popcorn = document.querySelector("#popcorn");
+const tooltip = document.querySelector("#tooltip");
+createPopper(popcorn, tooltip, {
+    placement: "top"
+});
